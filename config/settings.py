40e2abacc85db85656657a702412df6cfd59cc01
成田 <s1F102003537@iniad.org>
2022-11-09 16:07:07 +0900

    ファイル更新

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'team_app',
-    'accounts',
-    'authtest',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'js'
 
-TIME_ZONE = 'Tokyo'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
