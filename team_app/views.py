f163a3cd5d20d58648958f552cb62b70eed16b64
成田 <s1F102003537@iniad.org>
2022-12-07 16:21:24 +0900

    ＵRL接続１

--- team_app/views.py
+++ team_app/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
+from django.http import Http404
 import json
 
 # Create your views here.
 def home(request):
     return render(request, 'team_app/home.index', {})
 
+def index(request):
+    if request.method == "POST":
+        article = News(
+            title=request.POST['title'], body=request.POST['text'], grade=request.POST["grade"])
+        article.save()
+
+    try:
+        news = News.objects.order_by('-posted_at')
+    except News.DoesNotExist:
+        raise Http404('this objects does not exist.')
+
+    context = {
+        'news': news,
+    }
+
+    return render(request, 'team_app/home.html', context)
+
+
+def mypage(request, news_id):
+    try:
+        news = News.objects.get(pk=news_id)
+    except News.DoesNotExit:
+        raise Http404("blog does not exit")
+
+    context = {
+        "news": news
+    }
+
+    return render(request, 'team_app/mypage.html', context)
